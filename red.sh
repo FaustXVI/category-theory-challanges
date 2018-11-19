4da86591ecf21f78121ee4aab8b5030d95716541
FaustXVI <xavier.detant@gmail.com>
2018-11-19 17:06:37 +0100

    Add auto sync at red step

--- red.sh
+++ red.sh
@@ -3,7 +3,16 @@ function test() {
     ./gradlew test
 }
 
+function sync() {
+    git stash push -m "Rebasing"
+    git pull --rebase
+    git rebase -i --autosquash origin/master
+    git push
+    git stash pop
+}
+
 function commit() {
+    sync
     git add .
     git commit
     git tag -f lastRed
