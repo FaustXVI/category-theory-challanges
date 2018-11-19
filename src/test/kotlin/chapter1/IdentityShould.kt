65bbed1e30e3e07894cd0ee17a25ee5d3f3759db
FaustXVI <xavier.detant@gmail.com>
2018-11-19 17:09:05 +0100

    Composition should chain to int methods

--- src/test/kotlin/chapter1/IdentityShould.kt
+++ src/test/kotlin/chapter1/IdentityShould.kt
@@ -13,3 +13,17 @@ class IdentityShould {
 
     private fun <T> identity(value: T): T = value
 }
+
+
+class CompositionShould {
+
+    @Test
+    fun chainTwoFunctions() {
+        val times2: (Int) -> Int = { it * 2 }
+        val plus5: (Int) -> Int = { it + 5 }
+        assertk.assert(compose(times2, plus5)(1)).isEqualTo(7)
+    }
+
+}
+
+fun compose(g: (Int) -> Int, f: (Int) -> Int): (Int) -> Int = { f(g(it)) }
