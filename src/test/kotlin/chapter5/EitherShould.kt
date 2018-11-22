3c814d9d91369bca9a82f11b104b2ecf23268aee
FaustXVI <xavier.detant@gmail.com>
2018-11-22 21:05:15 +0100

    Show that j of question 5 is factorizable

--- src/test/kotlin/chapter5/EitherShould.kt
+++ src/test/kotlin/chapter5/EitherShould.kt
@@ -3,6 +3,8 @@ package chapter5
 import assertk.assert
 import assertk.assertions.isEqualTo
 import chapter1.compose
+import chapter5.Either.Left
+import chapter5.Either.Right
 import kotlin.test.Test
 
 
@@ -10,25 +12,28 @@ class EitherShould {
 
     @Test
     fun containsEitherType1OrType2() {
-        val left: Either<Int, String> = Either.Left(2)
-        val right: Either<Int, String> = Either.Right("value")
-        assert((left as Either.Left).v).isEqualTo(2)
-        assert((right as Either.Right).v).isEqualTo("value")
+        val left: Either<Int, String> = Left(2)
+        val right: Either<Int, String> = Right("value")
+        assert((left as Left).v).isEqualTo(2)
+        assert((right as Right).v).isEqualTo("value")
     }
 
     @Test
     fun question5() {
         val i: (Int) -> Int = { it }
-        val left: (Int) -> Either<Int, Boolean> = { v: Int -> Either.Left(v) }
+        val j: (Boolean) -> Int = { if (it) 0 else 1 }
+        val left: (Int) -> Either<Int, Boolean> = ::Left
+        val right: (Boolean) -> Either<Int, Boolean> = ::Right
 
         val m: (Either<Int, Boolean>) -> Int = {
             when (it) {
-                is Either.Left -> it.v
-                else -> 0
+                is Left -> it.v
+                is Right -> if (it.v) 0 else 1
             }
         }
 
         assert(compose(left, m)(42)).isEqualTo(i(42))
+        assert(compose(right, m)(false)).isEqualTo(j(false))
     }
 
 }
