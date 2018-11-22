136cf44320e0b01ac951421363673087cb3d5bc8
FaustXVI <xavier.detant@gmail.com>
2018-11-22 21:14:31 +0100

    Using question 6 to answer question 7

--- src/test/kotlin/chapter5/EitherShould.kt
+++ src/test/kotlin/chapter5/EitherShould.kt
@@ -49,6 +49,19 @@ class EitherShould {
         assert(compose(right, m)(false)).isEqualTo(j(false))
     }
 
+    @Test
+    fun question7() {
+        val i: (Int) -> Int = { if (it < 0) it else it + 2 }
+        val j: (Boolean) -> Int = { if (it) 0 else 1 }
+        val left: (Int) -> Either<Int, Boolean> = ::Left
+        val right: (Boolean) -> Either<Int, Boolean> = ::Right
+
+        val m: (Either<Int, Boolean>) -> Int = genericM(i, j)
+
+        assert(compose(left, m)(42)).isEqualTo(i(42))
+        assert(compose(right, m)(false)).isEqualTo(j(false))
+    }
+
 }
 
 fun genericM(i: (Int) -> Int, j: (Boolean) -> Int): (Either<Int, Boolean>) -> Int = {
