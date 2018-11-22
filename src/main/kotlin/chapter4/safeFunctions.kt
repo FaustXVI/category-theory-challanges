5e9b6ce32f0dc1c4f0a95e15456615032771a009
FaustXVI <xavier.detant@gmail.com>
2018-11-22 19:59:15 +0100

    Safe root gives same result as root

--- src/main/kotlin/chapter4/safeFunctions.kt
+++ src/main/kotlin/chapter4/safeFunctions.kt
@@ -1,3 +1,9 @@
 package chapter4
 
+import kotlin.math.sqrt
+
 fun safeReciprocal(n: Double): Maybe<Double> = if (n != 0.0) Maybe.Just(1.0 / n) else Maybe.Nothing()
+
+fun safeRoot(n: Double): Maybe<Double> {
+    return Maybe.Just(sqrt(n))
+}
--- src/test/kotlin/chapter4/SafeFunctions.kt
+++ src/test/kotlin/chapter4/SafeFunctions.kt
@@ -2,6 +2,7 @@ package chapter4
 
 import assertk.assert
 import assertk.assertions.isEqualTo
+import kotlin.math.sqrt
 import kotlin.test.Test
 
 class SafeFunctions {
@@ -15,5 +16,12 @@ class SafeFunctions {
     fun shouldGiveNothingForZero() {
         assert(safeReciprocal(0.0)).isEqualTo(Maybe.Nothing<Double>())
     }
+
+
+    @Test
+    fun shouldGiveRootValue() {
+        assert(safeRoot(2.0).value).isEqualTo(sqrt(2.0))
+    }
+
 
 }
