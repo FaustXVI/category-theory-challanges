498932886f9de28bc9930a32957df98bfb02b0a3
FaustXVI <xavier.detant@gmail.com>
2018-11-22 21:02:00 +0100

    Showing that i of question 5 is factorizable

--- src/test/kotlin/chapter5/EitherShould.kt
+++ src/test/kotlin/chapter5/EitherShould.kt
@@ -2,6 +2,7 @@ package chapter5
 
 import assertk.assert
 import assertk.assertions.isEqualTo
+import chapter1.compose
 import kotlin.test.Test
 
 
@@ -13,6 +14,21 @@ class EitherShould {
         val right: Either<Int, String> = Either.Right("value")
         assert((left as Either.Left).v).isEqualTo(2)
         assert((right as Either.Right).v).isEqualTo("value")
+    }
+
+    @Test
+    fun question5() {
+        val i: (Int) -> Int = { it }
+        val left: (Int) -> Either<Int, Boolean> = { v: Int -> Either.Left(v) }
+
+        val m: (Either<Int, Boolean>) -> Int = {
+            when (it) {
+                is Either.Left -> it.v
+                else -> 0
+            }
+        }
+
+        assert(compose(left, m)(42)).isEqualTo(i(42))
     }
 
 }
