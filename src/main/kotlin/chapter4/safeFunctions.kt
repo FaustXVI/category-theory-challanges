463b7289c624648463341e0bbea9d07a1939c582
FaustXVI <xavier.detant@gmail.com>
2018-11-22 20:02:24 +0100

    Safe square root give nothing for negatives

--- src/main/kotlin/chapter4/safeFunctions.kt
+++ src/main/kotlin/chapter4/safeFunctions.kt
@@ -4,6 +4,4 @@ import kotlin.math.sqrt
 
 fun safeReciprocal(n: Double): Maybe<Double> = if (n != 0.0) Maybe.Just(1.0 / n) else Maybe.Nothing()
 
-fun safeRoot(n: Double): Maybe<Double> {
-    return Maybe.Just(sqrt(n))
-}
+fun safeRoot(n: Double): Maybe<Double> = if (n >= 0) Maybe.Just(sqrt(n)) else Maybe.Nothing()
--- src/test/kotlin/chapter4/SafeFunctions.kt
+++ src/test/kotlin/chapter4/SafeFunctions.kt
@@ -17,10 +17,14 @@ class SafeFunctions {
         assert(safeReciprocal(0.0)).isEqualTo(Maybe.Nothing<Double>())
     }
 
-
     @Test
     fun shouldGiveRootValue() {
         assert(safeRoot(2.0).value).isEqualTo(sqrt(2.0))
+    }
+
+    @Test
+    fun shouldGiveNothingForNegativeValues() {
+        assert(safeRoot(-2.0)).isEqualTo(Maybe.Nothing<Double>())
     }
 
 
