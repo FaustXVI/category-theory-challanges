309fac04d68971a8cec8d10c70249f3c859c382e
FaustXVI <xavier.detant@gmail.com>
2018-11-26 17:19:08 +0100

    Add functions first and second to bifunctiors

--- src/test/kotlin/chapter8/Challanges.kt
+++ src/test/kotlin/chapter8/Challanges.kt
@@ -1,18 +1,24 @@
 package chapter8
 
+import assertk.assert
 import assertk.assertions.isEqualTo
+import chapter1.identity
 import kotlin.test.Test
 
 class Challanges {
     @Test
     fun pairIsABifunctor() {
         val myPair = MyPair(2, "toto")
-        assertk.assert(myPair.bimap({ it + 1 }, { it + "test" })).isEqualTo(MyPair(3, "tototest"))
+        assert(myPair.bimap({ it + 1 }, { it + "test" })).isEqualTo(MyPair(3, "tototest"))
+        assert(myPair.first { it + 1 }).isEqualTo(MyPair(3, "toto"))
+        assert(myPair.second { it + "test" }).isEqualTo(MyPair(2, "tototest"))
     }
 }
 
 interface Bifunctor<A, B> {
     fun <C, D> bimap(f: (A) -> C, g: (B) -> D): Bifunctor<C, D>
+    fun <C> first(f: (A) -> C): Bifunctor<C, B> = bimap(f, ::identity)
+    fun <D> second(g: (B) -> D): Bifunctor<A, D> = bimap(::identity, g)
 }
 
 data class MyPair<A, B>(val first: A, val second: B) : Bifunctor<A, B> {
