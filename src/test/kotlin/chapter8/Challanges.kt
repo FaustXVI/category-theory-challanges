0fdca9a78ccf319b029c2cb0508ea037bc2d16a1
FaustXVI <xavier.detant@gmail.com>
2018-11-26 17:41:05 +0100

    Prelist is a functor for Nil

--- src/test/kotlin/chapter8/Challanges.kt
+++ src/test/kotlin/chapter8/Challanges.kt
@@ -18,5 +18,20 @@ class Challanges {
         val myPair = MyPair(2, "toto")
         assert(myPair.fmap { it + "test" }).isEqualTo(MyPair(2, "tototest"))
     }
+
+    @Test
+    fun preListIsABifunctor() {
+        val emptyPreList: PreList<Int, String> = PreList.Nil()
+        assert(emptyPreList.bimap(Int::toString, String::toBoolean)).isEqualTo(PreList.Nil<String, Boolean>())
+    }
+}
+
+sealed class PreList<A, B> : Bifunctor<A, B> {
+    class Nil<A, B>() : PreList<A, B>() {
+        override fun <C, D> bimap(f: (A) -> C, g: (B) -> D): Bifunctor<C, D> = Nil()
+        override fun equals(other: Any?): Boolean = other is Nil<*, *>
+        override fun hashCode(): Int = 0
+
+    }
 }
 
