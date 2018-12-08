3d5910a2b90f1a15091128a483ce4454b779f8e9
FaustXVI <xavier.detant@gmail.com>
2018-12-08 11:39:26 +0100

    New natural transformation from Maybe to List that conserves values

--- src/test/kotlin/chapter9/NaturalTransformationShould.kt
+++ src/test/kotlin/chapter9/NaturalTransformationShould.kt
@@ -3,6 +3,7 @@ package chapter9
 import chapter4.Maybe
 import kotlin.test.Test
 import assertk.assert
+import assertk.assertions.contains
 import assertk.assertions.isEqualTo
 
 class NaturalTransformationShould {
@@ -15,8 +16,22 @@ class NaturalTransformationShould {
         checkNaturalityCondition(transformation, Maybe.Just(2), f)
     }
 
+    @Test
+    fun transformationToSingletonListIsANaturalTransformation() {
+        val transformation: (Maybe<Int>) -> List<Int> = ::maybeToList
+        val f: (Int) -> Int = { it * 2 }
+        assert(transformation(Maybe.Just(2))).contains(2)
+        checkNaturalityCondition(transformation, Maybe.Nothing(), f)
+        checkNaturalityCondition(transformation, Maybe.Just(2), f)
+    }
+
     private fun checkNaturalityCondition(transformation: (Maybe<Int>) -> List<Int>, nothing: Maybe<Int>, f: (Int) -> Int) {
         assert(transformation(nothing).map(f)).isEqualTo(transformation(nothing.map(f)))
     }
 
+}
+
+fun maybeToList(maybe: Maybe<Int>): List<Int> = when (maybe) {
+    is Maybe.Nothing -> emptyList()
+    is Maybe.Just -> listOf(maybe.value)
 }
