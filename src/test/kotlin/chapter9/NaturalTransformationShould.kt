8a886a3cfab1b83742f161ebe827edad0a1cfe30
FaustXVI <xavier.detant@gmail.com>
2018-12-08 12:05:14 +0100

    Define first transformation from reader to list

--- src/test/kotlin/chapter9/NaturalTransformationShould.kt
+++ src/test/kotlin/chapter9/NaturalTransformationShould.kt
@@ -5,6 +5,7 @@ import kotlin.test.Test
 import assertk.assert
 import assertk.assertions.contains
 import assertk.assertions.isEqualTo
+import chapter7.Reader
 
 class NaturalTransformationShould {
 
@@ -12,8 +13,10 @@ class NaturalTransformationShould {
     fun collapseToEmptyListIsANaturalTransformation() {
         val transformation: (Maybe<Int>) -> List<Int> = { emptyList() }
         val f: (Int) -> Int = { it * 2 }
-        checkNaturalityCondition(transformation, Maybe.Nothing(), f)
-        checkNaturalityCondition(transformation, Maybe.Just(2), f)
+        val maybe: Maybe.Nothing<Int> = Maybe.Nothing()
+        assert(transformation(maybe).map(f)).isEqualTo(transformation(maybe.map(f)))
+        val maybe1 = Maybe.Just(2)
+        assert(transformation(maybe1).map(f)).isEqualTo(transformation(maybe1.map(f)))
     }
 
     @Test
@@ -21,8 +24,10 @@ class NaturalTransformationShould {
         val transformation: (Maybe<Int>) -> List<Int> = { it.toList() }
         val f: (Int) -> Int = { it * 2 }
         assert(transformation(Maybe.Just(2))).contains(2)
-        checkNaturalityCondition(transformation, Maybe.Nothing(), f)
-        checkNaturalityCondition(transformation, Maybe.Just(2), f)
+        val maybe: Maybe.Nothing<Int> = Maybe.Nothing()
+        assert(transformation(maybe).map(f)).isEqualTo(transformation(maybe.map(f)))
+        val maybe1 = Maybe.Just(2)
+        assert(transformation(maybe1).map(f)).isEqualTo(transformation(maybe1.map(f)))
     }
 
     @Test
@@ -32,8 +37,13 @@ class NaturalTransformationShould {
         assert(Maybe.Just("2").toList().map(f)).isEqualTo(Maybe.Just("2").map(f).toList())
     }
 
-    private fun checkNaturalityCondition(transformation: (Maybe<Int>) -> List<Int>, maybe: Maybe<Int>, f: (Int) -> Int) {
-        assert(transformation(maybe).map(f)).isEqualTo(transformation(maybe.map(f)))
+
+    @Test
+    fun transformation1ofReader() {
+        val transformation: (Reader<Unit, Int>) -> List<Int> = { emptyList() }
+        val f: (Int) -> Int = { it * 2 }
+        val reader: Reader<Unit, Int> = Reader { 42 }
+        assert(transformation(reader).map(f)).isEqualTo(transformation(reader.fmap(f)))
     }
 
 }
