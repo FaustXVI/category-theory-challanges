baeeb913ef4b4a141c959f8a19daeb889d03a540
FaustXVI <xavier.detant@gmail.com>
2018-12-08 12:21:45 +0100

    Add second transfomation from reader bool to maybe

--- src/test/kotlin/chapter9/NaturalTransformationShould.kt
+++ src/test/kotlin/chapter9/NaturalTransformationShould.kt
@@ -74,4 +74,12 @@ class NaturalTransformationShould {
         assert(transformation(reader).fmap(f)).isEqualTo(transformation(reader.fmap(f)))
     }
 
+    @Test
+    fun transformation2ofReaderBool() {
+        val transformation: (Reader<Boolean, Int>) -> Maybe<Int> = { Maybe.Just(it(true))}
+        val f: (Int) -> Int = { it * 2 }
+        val reader: Reader<Boolean, Int> = Reader { if (it) 42 else 0 }
+        assert(transformation(reader).fmap(f)).isEqualTo(transformation(reader.fmap(f)))
+    }
+
 }
